e86d33a12a0a01fdb77b6d2bd1c321c73a1a1254
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-11-12 10:24:40 -0500

    hw

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  if (license === "none") {
+    return "";
+  } else {
+    return `(https://opensource.org/licenses/${license})`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `${renderLicenseBadge(license)}${renderLicenseLink(license)}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  * [Features](#features)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+
+  ## Deployed Application URL
+  ${data.link}
+
+## Description
+${data.description}  
+
+${renderLicenseSection(data.license)}
+
+## Creator
+${data.creator}
+
+## Require
+${data.require}
+
+## Features 
+${data.usage}
+
+## Email 
+${data.email}
+
+## Contributors 
+${data.contributors}
+
+## Test 
+${data.test}
 `;
+
+
+
 }
 
 module.exports = generateMarkdown;
