45126e3f0783991c860502972ce1b87a32e634a4
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-11-20 19:01:53 -0500

    correcting functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,3 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown");
 
 //not sure if this is ok
 
